c9e6104f39fbd2f8e89a2dac68d00996fdaaabaf
Waterticket <matthew218@naver.com>
2021-06-28 09:47:19 +0900

    초기 커밋.

---
+++ BatteryOverlay/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace BatteryOverlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.battery_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // battery_status
+            // 
+            this.battery_status.BackColor = System.Drawing.Color.Azure;
+            this.battery_status.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.battery_status.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.battery_status.Location = new System.Drawing.Point(0, 12);
+            this.battery_status.Name = "battery_status";
+            this.battery_status.Size = new System.Drawing.Size(250, 30);
+            this.battery_status.TabIndex = 0;
+            this.battery_status.Text = "100%  100%  24:00";
+            this.battery_status.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(250, 42);
+            this.Controls.Add(this.battery_status);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(600, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "BatteryStatus";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label battery_status;
+    }
+}
+
---
+++ BatteryOverlay/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteryOverlay
+{
+    public partial class Form1 : Form
+    {
+        bool On;
+        Point Pos;
+        private Timer MonitorTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MouseDown += (o, e) => { if (e.Button == MouseButtons.Left) { On = true; Pos = e.Location; } };
+            MouseMove += (o, e) => { if (On) Location = new Point(Location.X + (e.X - Pos.X), Location.Y + (e.Y - Pos.Y)); };
+            MouseUp += (o, e) => { if (e.Button == MouseButtons.Left) { On = false; Pos = e.Location; } };
+
+            this.BatteryChange();
+
+            this.MonitorTimer.Interval = 10000; // 10초에 한번씩 상태를 체크
+            this.MonitorTimer.Tick += new EventHandler(MonitorTimer_Tick);
+            this.MonitorTimer.Start();
+        }
+
+        void MonitorTimer_Tick(object sender, EventArgs e) 
+        { 
+            this.BatteryChange();
+        }
+
+        public void BatteryChange()
+        {
+            String fore_text = "";
+            ObjectQuery query = new ObjectQuery("Select * FROM Win32_Battery");
+
+            foreach (ManagementObject o in new ManagementObjectSearcher(query).Get())
+            {
+                //uint level = (uint)o.Properties["EstimatedChargeRemaining"].Value;
+                fore_text += o.Properties["EstimatedChargeRemaining"].Value + "%  ";
+            }
+
+            fore_text += DateTime.Now.ToString("HH:mm");
+
+            this.battery_status.Text = fore_text;
+        }
+    }
+}
